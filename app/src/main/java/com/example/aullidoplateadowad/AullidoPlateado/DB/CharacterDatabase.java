df31a2b15143d1714da7a67b2fcb2be8c247b1ee
dam2a <amateu@elpuig.xeill.net>
2019-11-28 19:00:57 +0100

    APWAD

--- app/src/main/java/com/example/aullidoplateadowad/AullidoPlateado/DB/CharacterDatabase.java
+++ app/src/main/java/com/example/aullidoplateadowad/AullidoPlateado/DB/CharacterDatabase.java
@@ -1,10 +1,13 @@
 package com.example.aullidoplateadowad.AullidoPlateado.DB;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Character.class}, views = {CharacterDetalle.class}, version = 1)
 public abstract class CharacterDatabase extends RoomDatabase {
@@ -15,8 +18,26 @@ public abstract class CharacterDatabase extends RoomDatabase {
 
     public static CharacterDatabase getInstance(final Context context){
         if(INSTANCE == null){
-            INSTANCE = Room.databaseBuilder(context, CharacterDatabase.class, "tareas-db").build();
+            INSTANCE = Room.databaseBuilder(context, CharacterDatabase.class, "tareas-db")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+                            insertarDatosIniciales(getInstance(context).characterDao());
+                        }
+                    })
+                    .build();
         }
         return INSTANCE;
+    }
+
+    static void insertarDatosIniciales(final CharacterDao characterDao){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                characterDao.insertarPersonaje(new Character("SORA", 0, 0, false));
+            }
+        });
     }
 }
